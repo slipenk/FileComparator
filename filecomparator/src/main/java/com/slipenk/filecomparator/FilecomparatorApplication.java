5cebf846b0183732131a41c738a9b017701c33c9
Yurii Slipenkyi <slipenk92@gmail.com>
2022-02-19 23:43:53 +0200

    Initial commit

---
+++ filecomparator/src/main/java/com/slipenk/filecomparator/FilecomparatorApplication.java
@@ -0,0 +1,13 @@
+package com.slipenk.filecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilecomparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilecomparatorApplication.class, args);
+	}
+
+}
---
+++ filecomparator/src/test/java/com/slipenk/filecomparator/FilecomparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slipenk.filecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilecomparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
