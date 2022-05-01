77ac1b99a9a474eb8a93f7ff68670bf0255b0950
Yurii Slipenkyi <slipenk92@gmail.com>
2022-05-01 23:38:50 +0300

    Update libraries

--- filecomparator-front/src/index.js
+++ filecomparator-front/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import { AuthProvider } from "./context/context";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
         <AuthProvider>
             <App />
         </AuthProvider>
     </React.StrictMode>,
-  document.getElementById('root')
 );
 
